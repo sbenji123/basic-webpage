a6bc149948c7e56c14d12a78076eca69c800e4f7
sbenji123 <sbenji123@gmail.com>
2022-04-17 18:45:05 -0500

    Purple Bingo Lose

--- js/app.js
+++ js/app.js
@@ -1,43 +1,46 @@
-console.log("hello world")
+console.log('hello world');
 
-document.addEventListener("DOMContentLoaded", function(){
-   console.log("is page loaded?")
-	// Vue, take control of SOME ELEMENT
-	new Vue({
-		template: `
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('is page loaded?');
+  // Vue, take control of SOME ELEMENT
+  new Vue({
+    template: `
 			<div>
 			A REACTIVE PAGE by {{name}}
 
-			<button @click="score">get point</button>
+			<button @click="addScore">get point</button>
 
 			points: {{points}}
+
+			<button @click="removeScore">lose point</button>
 			</div>
 		`,
-		el: "#app",
+    el: '#app',
 
-		methods: {
-			score() {
-				console.log("SCORE")
-				// Incret
-				this.points += 1
-			}
-		},
+    methods: {
+      addScore() {
+        console.log('SCORE');
+        // Incret
+        this.points += 1;
+      },
+      removeScore() {
+        this.points -= 1;
+      },
+    },
 
+    mounted() {
+      console.log('heelllloo?');
 
-		mounted() {
-			console.log("heelllloo?")
+      setInterval(() => {
+        console.log('Tick');
+        this.points += 1;
+      }, 300);
+    },
 
-			setInterval(() => {
-				console.log("Tick")
-				this.points += 1
-			}, 300)
-		},
-
-		data: {
-			points: 0,
-			name: "Kate",
-			emoji: ["💖"]
-		}
-	}) 
+    data: {
+      points: 0,
+      name: 'Me',
+      emoji: ['ahh'],
+    },
+  });
 });
-
